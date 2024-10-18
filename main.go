5d9bb7149eaeb1343f10cf62b52fafd498244150
陆离 <sirzenith@163.com>
2024-10-19 04:04:33 +0800

    update version to 0.2.0

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	cmd := &cli.Command{
 		Name:                  "bilinovel",
 		Usage:                 "helper program for downloading novels from www.bilinovel.com (mobile) or www.linovelib.com (desktop)",
-		Version:               "0.1.0",
+		Version:               "0.2.0",
 		EnableShellCompletion: true,
 		Commands: []*cli.Command{
 			book_dl.Cmd(),
